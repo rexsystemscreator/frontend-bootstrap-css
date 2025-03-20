4f186534db2a32e724f5d956e2403500144dd98c
Rex Systems <rexsystemscreator@gmail.com>
2025-03-19 23:37:32 -0400

    atualiza formulários de login e registro com placeholders e ajustes de estilo

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,30 +4,26 @@ function Login() {
   return (
     <div className="container-fluid">
       <div className="d-flex justify-content-center align-items-center vh-100">
-        <form className="col-lg-3 p-3 border border-1 rounded-3">
+        <form className="col-lg-3 p-4 border rounded-3">
           <h1 className="text-left">Entrar</h1>
           <p className="text-left">
             Preencha o formulário abaixo para fazer login.
           </p>
           <div className="mb-3">
-            <label htmlFor="inputEmail1" className="form-label">
-              Endereço de Email
-            </label>
             <input
               type="email"
               className="form-control"
               id="inputEmail1"
               aria-describedby="emailHelp"
+              placeholder="Email"
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="inputPassword1" className="form-label">
-              Senha
-            </label>
             <input
               type="password"
               className="form-control"
               id="inputPassword1"
+              placeholder="Senha"
             />
           </div>
           <button type="submit" className="btn btn-primary w-50">
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -4,36 +4,34 @@ function Register() {
   return (
     <div className="container-fluid">
       <div className="d-flex justify-content-center align-items-center vh-100">
-        <form className="col-lg-3 p-3 border border-1 rounded-3">
+        <form className="col-lg-3 p-4 border border-1 rounded-3">
           <h1 className="text-left">Registre-se</h1>
           <p className="text-left">
             Preencha o formulário abaixo para criar uma conta.
           </p>
           <div className="mb-3">
-            <label htmlFor="inputName1" className="form-label">
-              Nome Completo
-            </label>
-            <input type="text" className="form-control" id="inputName1" />
+            <input
+              type="text"
+              className="form-control"
+              id="inputName1"
+              placeholder="Nome"
+            />
           </div>
           <div className="mb-3">
-            <label htmlFor="inputEmail1" className="form-label">
-              Endereço de Email
-            </label>
             <input
               type="email"
               className="form-control"
               id="inputEmail1"
               aria-describedby="emailHelp"
+              placeholder="Email"
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="inputPassword1" className="form-label">
-              Senha
-            </label>
             <input
               type="password"
               className="form-control"
               id="inputPassword1"
+              placeholder="Senha"
             />
           </div>
 
